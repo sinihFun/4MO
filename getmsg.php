12603df7e78d0351f447cf7714d6e70877fcb65b
TSM <101980724+sinihFun@users.noreply.github.com>
2025-02-16 22:29:04 +0500

    Delete getmsg.php

--- getmsg.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $msg = htmlspecialchars($_POST['msg']);
-
-  echo  $msg
-?>
